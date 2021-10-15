c41b03f14e2e5f1e46a18db5fdcf746ef150cd56
Erick <erick.estay@virginiogomez.cl>
2021-10-15 10:38:24 -0300

    Initial commit

---
+++ app/src/main/java/cl/app/notas_alumno/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.app.notas_alumno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
