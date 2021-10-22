764fc0f7537261e0d8f62aecfcd1befc0761b192
Erick <erick.estay@virginiogomez.cl>
2021-10-22 17:12:59 -0300

    Versin 2.0

--- app/src/main/java/cl/app/notas_alumno/MainActivity.java
+++ app/src/main/java/cl/app/notas_alumno/MainActivity.java
@@ -12,38 +12,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText TPName, TPCarrera;
-    private EditText TNota1, TNota2, TNota3;
     private Button btCalcular;
+    private EditText TPName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TPName=(EditText) findViewById(R.id.TPName);
-        TPCarrera=(EditText)  findViewById(R.id.TPCarrera);
-        TNota1=(EditText)  findViewById(R.id.TNota1);
-        TNota2=(EditText)  findViewById(R.id.TNota2);
-        TNota3=(EditText)  findViewById(R.id.TNota3);
-        btCalcular=(Button)  findViewById(R.id.btCalcular);
+        TPName =(EditText)  findViewById(R.id.TPName);
+        btCalcular=(Button) findViewById(R.id.btCalcular);
 
-        Context contect=getApplicationContext();
+        Intent intentuno =new Intent(this,MainActivityDos.class);
 
 
-        Intent intentuno =new Intent(this, MainActivityN2.class);
+
 
         btCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intentuno.putExtra("Nombre", TPName.getText().toString());
-                intentuno.putExtra("Nota 1", TNota1.getText().toString());
-                intentuno.putExtra("Nota 2", TNota2.getText().toString());
-                intentuno.putExtra("Nota 3", TNota3.getText().toString());
-                startActivity(intentuno)
+                intentuno.putExtra("nombre", TPName.getText().toString());
+                startActivity(intentuno);
 
             }
-        })
+        });
 
 
     }
--- app/src/main/java/cl/app/notas_alumno/MainActivityDos.java
+++ app/src/main/java/cl/app/notas_alumno/MainActivityDos.java
@@ -3,6 +3,8 @@ package cl.app.notas_alumno;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,27 +13,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityDos extends AppCompatActivity  {
 
+        private Button btVolver;
+        private String TPName="";
         private TextView TxName;
-        private TextView TxNotaF;
+
+
 
         @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main_dos);
+                btVolver =(Button)  findViewById(R.id.btVolver);
+                TxName=(TextView)  findViewById(R.id.TxName);
+                Intent intent =new Intent(this, MainActivity.class);
 
-                TxName = (TextView)  findViewById(R.id.TxName);
-                TxNotaF = (TextView)  findViewById(R.id.TxNotaF);
-
-                Intent intent = new Intent(this,MainActivity.class );
-
-                Context context=getApplicationContext();
-                Bundle intent
-                Bundle intentE =this.getIntent().getExtras();
+                Context context=getApplicationContext()
+                Bundle intentE= this.getIntent().getExtras();
                 if (intentE!=null){
 
-                        TPName=intentE.getString("");
-                        Toast.makeText(contect, "Su nombre es"+TPName, Toast.LENGTH_SHORT).show();
-                } 
+                        TPName = intentE.getString("nombre");
+                        Toast.makeText(context, ""+TPName, Toast.LENGTH_SHORT).show();
+
+                }
+
+                TxName.setText("" +TxName);
+
+                btVolver.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                                startActivity(intent);
+                        }
+                });
 
         }
 }
